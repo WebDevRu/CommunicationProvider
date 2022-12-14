314a743ea7ed16a7d09aa260f40c6f633a9a11ea
nikrainev <nikrainev@gmail.com>
2022-12-15 00:04:19 +0300

    feat: add tarif form

---
+++ app/Core/Services/InternetTariff.cs
@@ -0,0 +1,57 @@
+namespace app.Core.Services;
+using app.Core.Model;
+using System.Text.Json;
+
+enum ServiceTypes
+{
+    InternetTV,
+    CabelInternet,
+    Cellular,
+}
+
+enum TariffStatuses
+{
+    Active,
+    Archived,
+}
+
+public struct AddTariffArgs
+{
+    public int SpeedMb;
+    public string Title;
+    public string Description;
+    public int PeriodPaymentAmount;
+    public int PeriodSeconds;
+}
+
+public class InternetTariff
+{
+    MasterContext dbContext = new ();
+    
+    public void AddTariff(AddTariffArgs args)
+    {
+        var limit = new Limit
+        {
+            CableInternetSpeedMbs = args.SpeedMb, 
+            IsSystem = false, 
+            ServiceType = JsonSerializer.Serialize(ServiceTypes.CabelInternet),
+        };
+        dbContext.Add<Limit>(limit);
+        dbContext.SaveChanges();
+
+        var tariff = new Tariff
+        {
+            LimitId = limit.Id,
+            Title = args.Title,
+            Description = args.Description,
+            InitialPaymentAmount = 0,
+            PeriodSeconds = args.PeriodSeconds,
+            PeriodPaymentAmount = args.PeriodPaymentAmount,
+            ServiceType = JsonSerializer.Serialize(ServiceTypes.CabelInternet),
+            Status = JsonSerializer.Serialize(TariffStatuses.Active),
+        };
+
+        dbContext.Add<Tariff>(tariff);
+        dbContext.SaveChanges();
+    }
+}
